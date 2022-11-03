3b23103b22e90d8e6fb27e8041406caeb0fb350a
kersh2000 <bradleykershaw333@gmail.com>
2022-11-03 16:04:23 +0000

    Created tests for the Cheese model

--- tests/tablesCreated.test.js
+++ tests/tablesCreated.test.js
@@ -3,7 +3,7 @@ const db = require('../db/db');
 
 describe('Can create multiple rows within the Board model/table', () => {
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     await db.sync({
       force: true
     });
@@ -57,9 +57,61 @@ describe('Can create multiple rows within the Board model/table', () => {
   });
 });
 
+describe('Can create multiple rows within the Cheese model/table', () => {
+
+  beforeAll(async () => {
+    await db.sync({
+      force: true
+    });
+    await Cheese.create({
+      title: 'Camembert',
+      description: 'Rich, buttery, and spreadable, Camembert has a mild, mushroomy aroma.'
+    });
+  });
+
+  test('Can successfully create a single row within Cheese', async () => {
+    const numOfRows = await Cheese.count();
+    expect(numOfRows).toBe(1);
+  });
+
+  test('Title is successfully created within each row', async () => {
+    const cheese = await Cheese.findOne();
+    expect(cheese.title).toBe('Camembert');
+  });
+
+  test('Description is successfully created within each row', async () => {
+    const cheese = await Cheese.findOne();
+    expect(cheese.description).toBe('Rich, buttery, and spreadable, Camembert has a mild, mushroomy aroma.');
+  });
+
+  test('Can create multiple rows within Cheese', async () => {
+    await Cheese.bulkCreate([
+      {
+        title: 'Cotija',
+        description: 'As the cheese ages, it acquires nuttier, tangier flavors and a drier, coarser texture.'
+      },
+      {
+        title: 'Feta',
+        description: 'Tangy and moist, feta can range from completely crumbly to moderately creamy and pairs well with fresh summer fruit.'
+      },
+      {
+        title: 'Mozzarella',
+        description: 'Fresh and dairy-rich, mozzarella is prized for its texture and mild creamy flavor.'
+      }
+    ]);
+    const cheeses = await Cheese.findAll();
+    expect(cheeses[1].title).toBe('Cotija');
+    expect(cheeses[1].description).toBe('As the cheese ages, it acquires nuttier, tangier flavors and a drier, coarser texture.');
+    expect(cheeses[2].title).toBe('Feta');
+    expect(cheeses[2].description).toBe('Tangy and moist, feta can range from completely crumbly to moderately creamy and pairs well with fresh summer fruit.');
+    expect(cheeses[3].title).toBe('Mozzarella');
+    expect(cheeses[3].description).toBe('Fresh and dairy-rich, mozzarella is prized for its texture and mild creamy flavor.');
+  });
+});
+
 describe('Can create multiple rows within the User model/table', () => {
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     await db.sync({
       force: true
     });
