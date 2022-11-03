cb83031684a511b65faec991f5fc624ed5fdae10
kersh2000 <bradleykershaw333@gmail.com>
2022-11-03 22:27:35 +0000

    Created file to improve testing with pre-data

---
+++ db/data.js
@@ -0,0 +1,38 @@
+const { Board, Cheese, User } = require('../models/index');
+const db = require('./db');
+
+class Data {
+  
+  static content = new Array();
+  static models = ['Board', 'Cheese', 'User'];
+  static objs = [Board, Cheese, User];
+
+  static async new() {
+
+    for (let i = 0; i < Data.models.length; i++) {
+      const obj = await Data.objs[i].findAll();
+      const data = obj.map(x => x.toJSON());
+      Data.content.push(data);
+    }
+
+    await db.sync({
+      force: true
+    });
+
+  }
+
+  static async reset() {
+
+    await db.sync({
+      force: true
+    });
+
+    for (let i = 0; i < Data.models.length; i++) {
+      await Data.objs[i].bulkCreate(Data.content[i]);
+    }
+
+  }
+
+}
+
+module.exports = Data;
