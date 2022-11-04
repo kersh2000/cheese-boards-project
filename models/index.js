230f53585ca1c426e0cc136b6a62ffe93f4d9467
kersh2000 <bradleykershaw333@gmail.com>
2022-11-04 09:53:15 +0000

    Added relationships between tables

--- models/index.js
+++ models/index.js
@@ -4,15 +4,16 @@ const User = require('./user.model');
 
 /****** Here, we are setting up our connections/relationships between tables *******/
 
-
-//ONE-TO-ONE - The target belongs to the source, and the source has the target
-
-
-
 //ONE-TO-MANY - The targets belong to the source, and the soruce has many targets
+
+User.hasMany(Board);
+Board.belongsTo(User);
 
 
 //MANY-TO-MANY - Both the target belong to many sources and the source belongs to many targets, which are connected through the junction/through table (by thier ID's).
+
+Cheese.belongsToMany(Board);
+Board.belongsToMany(Cheese);
 
 
 module.exports = { Board, Cheese, User }
