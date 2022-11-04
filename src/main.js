2606367d670165515b9b79f3adbdd09a5df2dede
kersh2000 <bradleykershaw333@gmail.com>
2022-11-04 15:27:28 +0000

    Added sample data to test reset

--- src/main.js
+++ src/main.js
@@ -1,7 +1,43 @@
-const seed = require('../db/seed');
+const { Board, Cheese, User } = require('../models/index');
+const db = require('../db/db');
+const { Op } = require('sequelize')
 
 async function main() {
-  await seed();
+
+  await db.sync({
+    force: true
+  });
+
+  const boards = await Board.bulkCreate([
+    {
+      type: 'Hard cheeses',
+      description: `You'll need a knife`,
+      rating: 9
+    }
+  ]);
+
+  const cheeses = await Cheese.bulkCreate([
+    {
+      title: 'cheese1',
+      description: 'Its tasty!!!'
+    },
+    {
+      title: 'cheese2',
+      description: 'Its very tasty!!!'
+    }
+  ]);
+
+  const users = await User.bulkCreate([
+    {
+      name: 'Homer Simpson',
+      email: 'hommer@hotmail.com'
+    },
+    {
+      name: 'Bradley Kershaw',
+      email: 'brad@google.com'
+    }
+  ]);
+
 }
 
 main();
