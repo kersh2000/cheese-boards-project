1db973801e953df3b7ea0e38caf0e39625a7aa68
kersh2000 <bradleykershaw333@gmail.com>
2022-11-04 12:17:53 +0000

    Added through table name

--- models/index.js
+++ models/index.js
@@ -12,8 +12,8 @@ Board.belongsTo(User);
 
 //MANY-TO-MANY - Both the target belong to many sources and the source belongs to many targets, which are connected through the junction/through table (by thier ID's).
 
-Cheese.belongsToMany(Board);
-Board.belongsToMany(Cheese);
+Cheese.belongsToMany(Board, { through: 'Board_Cheese' });
+Board.belongsToMany(Cheese, { through: 'Board_Cheese' });
 
 
 module.exports = { Board, Cheese, User }
